56cc000a0206fcbedbd2e4ab6879a3ec042a1dfa
Saii206 <scainginalec@gmail.com>
2026-05-01 14:41:00 +0800

    Student D: update mode

--- main.py
+++ main.py
@@ -20,3 +20,21 @@ def view_contacts():
     with open("contacts.txt", "r") as file:
         for line in file:
             print(line.strip())
+
+def update_contact():
+    name = input("Enter name to update: ")
+    new_number = input("Enter new number: ")
+
+    lines = []
+
+    with open("contacts.txt", "r") as file:
+        for line in file:
+            if name in line:
+                lines.append(f"{name} - {new_number}\n")
+            else:
+                lines.append(line)
+
+    with open("contacts.txt", "w") as file:
+        file.writelines(lines)
+
+    print("Updated!")            
